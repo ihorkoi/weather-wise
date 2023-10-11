9f33787c6f4b51071fe268e353fd8a82215deb3f
ihorkoi <koleev.igor@gmail.com>
2023-10-11 17:40:06 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,7 @@ export const App = () => {
         <img src={logo} className="App-logo" alt="logo" />
         <h2>Welcome to React</h2>
       </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
+      <p className="App-intro"></p>
       <WeatherList />
     </div>
   );
